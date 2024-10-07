4303be89cfaa052fc593e8940f8e0a9c80688b40
Khoa học dữ liệu 17a3hn <hnshai.dhkl17a3hn@sv.uneti.edu.vn>
2024-10-07 23:20:24 +0700

    bài tập chương 2

---
+++ Bai2.5.py
@@ -0,0 +1,19 @@
+#NguyenXuanHuy
+import xml.dom.minidom
+def main():
+    doc=xml.dom.minidom.parse("sample.xml")
+    print(doc.nodeName)
+    print(doc.firstChild.tagName)
+#---------------
+    staff = doc.getElementsByTagName("staff")
+    print("%d staff:" % staff.length) 
+    for information in staff: 
+        name=information.getElementsByTagName('name')
+        salary=information.getElementsByTagName('salary')
+        print("Staff id =",information.getAttribute("id"))
+        for i in name:
+            print(i.firstChild.data)
+        for i in salary:
+            print(i.firstChild.data)
+if __name__=="__main__": 
+   main()
